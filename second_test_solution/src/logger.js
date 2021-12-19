2725940c9ca00873cf80e7f387abb1a3be80834d
Rafal Witczak <rwitczak89@gmail.com>
2021-12-19 19:36:15 +0100

    fix: fix prettyfied logs

--- second_test_solution/src/logger.js
+++ second_test_solution/src/logger.js
@@ -2,9 +2,21 @@ const pino = require('pino');
 
 const logger = pino({
   name: 'CourierAPI',
-  prettyPrint: true,
+  level: process.env.LOG_LEVEL || 'info',
   messageKey: 'message',
   timestamp: pino.stdTimeFunctions.isoTime,
+  transport:
+    String(process.env.LOG_PRETTY) === 'true'
+      ? { target: 'pino-pretty', options: { colorize: true } }
+      : null,
+  serializers: {
+    req(req) {
+      return {
+        url: req.url.split('?')[0],
+        query: req.query,
+      };
+    },
+  },
 });
 
 module.exports = { logger };
